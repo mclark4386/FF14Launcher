2266e6140610d65d355018c33dc111fcec4f6338
Arthur Moore <Arthur.Moore.git@cd-net.net>
2015-09-28 01:44:44 -0400

    Make gen_launcher_string not rely on global variables
    User now has the (unsafe) option of storing their password in the file
    Minor whitespace cleanups

--- launcher.py
+++ launcher.py
@@ -11,6 +11,7 @@ user = "user"
 path = "/" #Path containing boot and game
 wine_command = 'wine' #Prefix execution with 'wine' (for Linux/Mac)
 #wine_command = ''  #For Windows
+password = ''
 one_time_password = ''
 
 import urllib2
@@ -22,30 +23,32 @@ import sys
 import ssl
 from getpass import getpass
 
-passwd = getpass()
+if (password == ''):
+    password = getpass()
 
-context = ssl._create_unverified_context()
 
-def gen_hash(file):
-	return str(os.stat(file).st_size) + "/" + hashlib.sha1(open(file, "rb").read()).hexdigest()
 
 if len(sys.argv) > 1:
 	one_time_password = sys.argv[1]
 if len(sys.argv) > 2:
 	user = sys.argv[2]
-	passwd = sys.argv[3]
+	password = sys.argv[3]
 
-def gen_launcher_string(username,password,otpw,gamepath):
+def gen_hash(file):
+	return str(os.stat(file).st_size) + "/" + hashlib.sha1(open(file, "rb").read()).hexdigest()
+
+def gen_launcher_string(region,username,password,otpw,gamepath):
+	context = ssl._create_unverified_context()
+
 	os.chdir(gamepath + "/game")
 
 	version = ""
 	with open('ffxivgame.ver', 'r') as f:
 		version = f.readline()
-	
+
 	headers = {"User-Agent":"SQEXAuthor/2.0.0(Windows 6.2; ja-jp; ecf4a84335)"}
 	login_url = "https://ffxiv-login.square-enix.com/oauth/ffxivarr/login/top?lng=en&rgn="+region
-	login_info_req = urllib2.Request(login_url,
-				 None, headers)
+	login_info_req = urllib2.Request(login_url, None, headers)
 	
 	login_info = urllib2.urlopen(login_info_req)
 	cookies = login_info.headers.get('Set-Cookie')
@@ -54,35 +57,36 @@ def gen_launcher_string(username,password,otpw,gamepath):
 	m = re.search('<input type="hidden" name="_STORED_" value="(.*)"', response)
 	headers = {"User-Agent":"SQEXAuthor/2.0.0(Windows 6.2; ja-jp; ecf4a84335)", "Cookie": cookies,
 		   "Referer": login_url, "Content-Type": "application/x-www-form-urlencoded"}
-	#print("user:"+username+" pass:"+password+" otpw:"+otpw)
+
 	login_data = urllib.urlencode({'_STORED_':m.group(1), 'sqexid':username, 'password':password, 'otppw':otpw})
 	login_req = urllib2.Request("https://ffxiv-login.square-enix.com/oauth/ffxivarr/login/login.send",
 				    login_data, headers)
-	
+
 	login_result = urllib2.urlopen(login_req)
 	response = login_result.read()
 	m = re.search('login=auth,ok,sid,(.+?),', response)
 	if not m:
-            #Login failed. Abort
-		print("Login failed. Please try again.")
-		quit()
-	
+		raise Exception("Login failed. Please try again.")
+
 	sid = m.group(1)
 
-#gamever headers
+	#gamever headers
 	headers = {"X-Hash-Check":"enabled"}
-#Use the patch gamever service to retrieve our *actual* sid.
+
+	#Use the patch gamever service to retrieve our *actual* sid.
 	gamever_url = "https://patch-gamever.ffxiv.com/http/win32/ffxivneo_release_game/"+version+"/"+sid
 
-#calculate hashes...
+	#calculate hashes...
 	hash_str = "ffxivboot.exe/" + gen_hash("../boot/ffxivboot.exe") + "," + "ffxivlauncher.exe/" + gen_hash("../boot/ffxivlauncher.exe") + "," + "ffxivupdater.exe/" + gen_hash("../boot/ffxivupdater.exe")
 
 	gamever_req = urllib2.Request(gamever_url, hash_str, headers)
-	
 	gamever_result = urllib2.urlopen(gamever_req, context=context)
 	actual_sid = gamever_result.info().getheader("X-Patch-Unique-Id")
-	return (wine_command + ' ffxiv.exe "DEV.TestSID='+ actual_sid + '" "DEV.UseSqPack=1" "DEV.DataPathType=1" "DEV.LobbyHost01=neolobby01.ffxiv.com" "DEV.LobbyPort01=54994" "DEV.LobbyHost02=neolobby02.ffxiv.com" "DEV.LobbyPort02=54994" "SYS.Region=3" "language=1" "ver='+version+'"')
+	return ('ffxiv.exe "DEV.TestSID='+ actual_sid + '" "DEV.UseSqPack=1" "DEV.DataPathType=1" "DEV.LobbyHost01=neolobby01.ffxiv.com" "DEV.LobbyPort01=54994" "DEV.LobbyHost02=neolobby02.ffxiv.com" "DEV.LobbyPort02=54994" "SYS.Region=3" "language=1" "ver='+version+'"')
 
-launch = gen_launcher_string(user,passwd,one_time_password,path)
-print(launch)
-os.system(launch)
+try:
+	launch = wine_command + ' ' + gen_launcher_string(region,user,password,one_time_password,path)
+	print(launch)
+	os.system(launch)
+except Exception, err:
+	print("Error:  " + str(err))
